438fd6858f68154d99205e297fbdb2cac9d26790
sticky <carina_profe@hotmail.com.ar>
2016-07-20 21:36:26 -0300

    base miercoles 20/jul/16

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<?php include 'inc/head_common.php'; ?>
 	<link rel="stylesheet" href="css/workshops.css">
 	<title>AddMy</title>
@@ -21,12 +22,13 @@
 						<h2>¿Necesitás tu página web hoy?</h2>
 					</div>
 					<div class="col-xs-12 main-info">
-						<a href="#" id="btn_comenzar" class="btn btn-custom">COMENZAR!</a>
+						<a href="#" id="btn_comenzar" class="btn btn-custom btn_comenzar">COMENZAR!</a>
 					</div>	
 				</div>
 			</div>
 		</article>
 		<div id="paso1"></div>
+		<div class="clearfix"></div>
 		<article id="w2" class="workshop workshop-right">
 			<div class="container">
 				<div class="row">
@@ -37,13 +39,14 @@
 				<div class="clearfix"></div>
 				<form id="subscribe-form" action="">
 					<div class="row">
-						<div class="col-xs-7 col-sm-5 col-sm-offset-2">
-							<div class="form-group">
-								<input type="email" id="email" name="email" class="form-control input-lg" placeholder="Tu correo electrónico" />
-							</div>
+						<div class="col-xs-12 col-sm-6 col-sm-offset-3 margen-sup-input">
+								<input type="email" class="form-control input-lg" id="email" placeholder="Tu correo">
+								<input type="text" class="form-control input-lg" id="nombre" placeholder="Tu nombre">
+								<input type="password" class="form-control input-lg" id="password" placeholder="Tu contraseña">
+								<input type="password" class="form-control input-lg" id="password2" placeholder="Ingresa nuevamente tu contraseña">
 						</div>
-						<div class="col-xs-5 col-sm-3 ">
-							<button id='btn_login' class="btn btn-custom btn-lg">SIGUIENTE!</button>
+						<div class="col-xs-12 ">
+							<button id="btn_login" class="btn btn-custom btn-lg">CONTINUAR!</button>
 						</div>
 					</div>	
 				</form>	
@@ -57,25 +60,89 @@
 					<div class="col-xs-12 main-info">
 						<h2>2. Elige un lindo nombre para tu sitio!</h2>
 					</div>
-					<div class="clearfix"></div>
+				</div>
+				<div class="clearfix"></div>
 					<form id="subscribe-form" action="">
 						<div class="row">
-							<div class="col-xs-7 col-sm-5 col-sm-offset-2">
-								<div class="form-group">
-									<input type="text" id="nombre" class="form-control input-lg" placeholder="Nombre del sitio" />
-								</div>
+							<div class="col-xs-8 col-sm-5 col-sm-offset-3 margen-sup-input">
+								<input type="text" class="form-control input-lg" id="siteName" placeholder="Nombre del sitio" name="siteName"> 
+								<span id="msgDisponible" class="help-block aIzq">Ej. tusitio.addmy.site</span>
 							</div>
-							<div class="col-xs-5 col-sm-3 ">
+							<div class="col-xs-4 col-sm-3 ">
 								<h3>.addmy.site</h3>
 							</div>
-						</div>	
+						</div>
+						<div class="col-xs-12 col-sm-5 col-sm-offset-3">
+							<h3>Tu dirección de correo es: &nbsp;&nbsp;<span id="emailSite"> tusitio@addmy.site</span></h3>
+						</div>
+						<div class="col-xs-12 ">
+							<button id="btn-continuar1" class="btn btn-custom btn-lg btn-continuar">CONTINUAR!</button>
+						</div>
 					</form>	
+			</div>
+		</article>
+		<div id="paso4"></div>
+		<div class="clearfix"></div>
+		<article id="w5" class="workshop workshop-left">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 main-info">
+						<h2>4. ¿Que van a ver en tu sitio?</h2>
+					</div>
+				</div>
+
+				<div class="clearfix"></div>
+
+				<div class="row marginTop paso4">
+
+						<div class="col-md-4">
+						    <label for="vh1">
+						      	<input type="checkbox" name="ch1" id="ch1" value="option1">Logo
+							</label>
+						</div>
+
+						<div class="col-md-4">
+						    <label for="vh1">
+						      	<input type="checkbox" name="ch1" id="ch1" value="option1">Logo
+							</label>
+						</div>
+
+						<div class="col-md-4">
+						    <label for="vh1">
+						      	<input type="checkbox" name="ch1" id="ch1" value="option1">Logo
+							</label>
+						</div>
+
+						<div class="col-md-4">
+						    <label for="vh1">
+						      	<input type="checkbox" name="ch1" id="ch1" value="option1">Logo
+							</label>
+						</div>
+
+						<div class="col-md-4">
+						    <label for="vh1">
+						      	<input type="checkbox" name="ch1" id="ch1" value="option1">Logo
+							</label>
+						</div>
+
+						<div class="col-md-4">
+						    <label for="vh1">
+						      	<input type="checkbox" name="ch1" id="ch1" value="option1">Logo
+							</label>
+						</div>
+					
+				</div>
+				<div class="clearfix"></div>
+				<div class="row">
+					<div class="col-xs-12 main-info">
+						<button id="btn_continuar2" class="btn btn-custom btn-lg btn-continuar">CONTINUAR!</button>
+					</div>	
 				</div>
 			</div>
 		</article>
 	</div>
 
-	<?php include 'inc/footer.php'; ?>
+	<?php //include 'inc/footer.php'; ?>
 	<?php include 'inc/footer_common.php'; ?>
 </body>
 </html>
--- js/function.js
+++ js/function.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {    
+$("#w2").hide( "fast" );
+$("#w3").hide( "fast" );
+$("#w5").hide( "fast" );
 
 	$("#btn_comenzar").click(function(event) {
 		event.preventDefault();
+		$("#w2").show( "slow" );
 		$('html, body').animate({
 			scrollTop: $("#paso1").offset().top
 		}, 500);
@@ -10,7 +14,7 @@ $(document).ready(function() {
 
 	$("#btn_login").click(function(event) {
 		event.preventDefault();
-		if( !isValidEmailAddress( $("#email").val() ) ) { /* do stuff here */ 
+		if( !isValidEmailAddress( $("#email").val() ) ) { 
 			$("#email").css({
 				border: '1px solid red'
 			});
@@ -18,21 +22,36 @@ $(document).ready(function() {
 			$('html, body').animate({
 				scrollTop: $("#paso2").offset().top
 			}, 500);
+			$("#w3").show( "slow" );
 			$("#email").css({
 				border: '0px solid black'
 			});
 		}
+		
 	});
 
-	$("#btn_registro").click(function(event) {
+	
+
+	$("#btn-continuar1").click(function(event) {
 		event.preventDefault();
+		$("#w5").show( "slow" );
 		$('html, body').animate({
-			scrollTop: $("#paso1").offset().top
+			scrollTop: $("#paso4").offset().top
 		}, 500);
 	});
-function isValidEmailAddress(emailAddress) {
+
+	$("#siteName").on("input propertychange",function(){
+		if($("#siteName").val() == ""){
+			$("#emailSite").html("tusitio@addmy.site");
+		}else{
+			$("#emailSite").html($("#siteName").val() + "@addmy.site");
+		}
+	
+	});
+
+	function isValidEmailAddress(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
-}
+	}
 
 });
